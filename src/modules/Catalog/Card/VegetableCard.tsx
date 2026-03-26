f55e60339f9708d1314e19db6a8aa548b98f28bc
Vladislav-Filatov <vf140704@gmail.com>
2026-03-26 23:07:04 +0300

    Добвил счетчик на карточки

--- src/modules/Catalog/Card/VegetableCard.tsx
+++ src/modules/Catalog/Card/VegetableCard.tsx
@@ -1,6 +1,9 @@
-import { Card, Image, Text, Button, Group } from '@mantine/core';
+import { Card, Image, Text, Button, Group, ActionIcon  } from '@mantine/core';
 import styles from './style.module.scss'
 import CartIcon from "../../../assets/cart.svg?react"
+import MinusIcon from '../../../assets/minus-button.svg?react';
+import PlusIcon from '../../../assets/plus-button.svg?react';
+import {useState} from "react";
 
 type VegetableCardProps = {
   name: string;
@@ -9,6 +12,16 @@ type VegetableCardProps = {
 }
 
 export const VegetableCard = ({name, price, image}: VegetableCardProps) => {
+  const [count, setCount] = useState<number>(1);
+
+  const handlePlusCount = () => {
+    setCount(prev => prev +1);
+  }
+
+  const handleMinusCount = () => {
+    setCount(prev => Math.max(1, prev -1));
+  }
+
   return (
     <Card className={styles.card} shadow="sm" padding="lg" radius="md">
       <Group>
@@ -21,11 +34,28 @@ export const VegetableCard = ({name, price, image}: VegetableCardProps) => {
 
       <Group justify="space-between" mt="md" mb="xs">
         <Text fw={500}>{name}</Text>
+        <Group gap={7}>
+          <ActionIcon
+            variant="subtle"
+            radius="md"
+            onClick={handleMinusCount}
+          >
+            <MinusIcon />
+          </ActionIcon>
+          <Text>{count}</Text>
+          <ActionIcon
+            variant="subtle"
+            radius="md"
+            onClick={handlePlusCount}
+          >
+            <PlusIcon />
+          </ActionIcon>
+        </Group>
       </Group>
 
       <Group wrap="nowrap" justify="space-between">
         <Text fw={500} style={{ flexShrink: 0 }}>$ {price}</Text>
-        <Button fullWidth className={styles['add-button']} color="#E7FAEB"radius="md" rightSection={<CartIcon/>}>
+        <Button fullWidth className={styles['add-button']} color="#E7FAEB" radius="md" rightSection={<CartIcon/>}>
           Add to cart
         </Button>
       </Group>
